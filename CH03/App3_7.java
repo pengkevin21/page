f1f172e015825d0fec4445ade8e71908ca2dfed0
彭鉅凱 <62436233+pengkevin21@users.noreply.github.com>
2021-11-17 23:33:46 +0800

    Add files via upload

---
+++ CH03/App3_7.java
@@ -0,0 +1,10 @@
+public class App3_7
+{
+	public static void main (String args[])
+	{
+		char ch='\"';
+		
+		System.out.println(ch+"Spring is coming!"+ch );
+		System.out.println("\"Birds are flying!\"");
+	}
+}
