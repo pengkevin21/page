3e0913a2b988b3c5b1630a3a2f018bc156511270
彭鉅凱 <62436233+pengkevin21@users.noreply.github.com>
2021-11-17 23:25:21 +0800

    Add files via upload

---
+++ CH02/App2_1.java
@@ -0,0 +1,11 @@
+// App2_1, 簡單的Java程式
+public class App2_1
+{
+	public static void main (String args[])
+	{
+		int num;
+		num=2;
+		System.out.println("I have "+num+" dogs");
+		System.out.println("You have "+num+" dogs, too");
+	}
+}
